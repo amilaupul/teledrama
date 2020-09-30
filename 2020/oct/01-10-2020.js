db3f994081782dd7e0d8ee2a200d9d4d4c54991e
amilaupul <57644319+amilaupul@users.noreply.github.com>
2020-10-01 05:02:28 +0530

    Create 01-10-2020.js

---
+++ 2020/oct/01-10-2020.js
@@ -0,0 +1,75 @@
+const auk_other_tele_image = {
+        seasonTicket: 'https://i.ibb.co/yYYXsbs/Seesan-Tiket-Episode.jpg',//Season Ticket - 0
+        aladdin: 'https://i.ibb.co/c21GY6k/Aladin-episodes.jpg',//Aladdin - 1
+        praveena2: 'https://i.ibb.co/6rVPdtC/praveena-episode-2020-July-min.png',//Praveena-2 - 2
+        meAdarayai: 'https://i.ibb.co/TR6Bmq9/Me-Adarayai-Episodes.jpg',//Me Adarayai - 3
+        sithAhaseAdaren: 'https://i.ibb.co/9tgC4kL/sith-ahase-adaren.jpg',//Sith Ahase Adaren - 4
+        adareiManAdarei: 'https://i.ibb.co/HPzT3rg/Adarei-Man-Adarei-Episode-2020-July-min.png',//Adarei Man Adarei - 5
+        miniPahanaObai: 'https://i.ibb.co/DYQjm1r/mini-pahana-obai-episode.jpg',//Mini Pahana Obai - 6
+        dharani: 'https://i.ibb.co/gTQNZgX/f-Iu2-Cz1bs-Johq.jpg',//Dharani - 7
+        agra: 'https://i.ibb.co/SK2rK84/JRIHn-Fk-Kjq8hd.jpg',//Agra - 8
+        ahasMaliga: 'https://i.ibb.co/THWDhRC/Ahas-Maliga.jpg',//Ahas Maliga - 9
+        deweniInima: 'https://i.ibb.co/VqWfRvc/Deweni-Inima-Episode-2020-July-min.png',//Deweni Inima - 10
+        sangeethe: 'https://i.ibb.co/zmnRxRb/Sangeethe.jpg',//Sangeethe - 11
+        neelaPabalu: 'https://i.ibb.co/KrzHxRX/Neela-Pabalu.jpg',//Neela Pabalu - 12
+        sihinaSamagama: 'https://i.ibb.co/m6hsQ4M/SIHINA-SAMAGAMA.jpg',//Sihina Samagama - 13
+        hadawatheKathawa: 'https://i.ibb.co/y6dxRrB/Hadawathe-Kathawa.jpg',//Hadawathe Kathawa - 14
+        akuruMakiNa: 'https://i.ibb.co/q5L0LKG/Akuru-Maki-Na.jpg',//Akuru Maki Na - 15
+        mahaViruPandu: 'https://i.ibb.co/hRvmF7G/Maha-Viru-Pandu-jpg.jpg',//Maha Viru Pandu - 16
+        pulingu: 'https://i.ibb.co/p3bg7pz/pulingu.jpg',//Pulingu - 17
+        sihini: 'https://i.ibb.co/858Xj6q/Sihini.jpg',//Sihini - 18
+        sakalaGuru: 'https://i.ibb.co/6Nc0vNk/Sakala-Guru.jpg',//Sakala Guru - 19
+        hondaWade: 'https://i.ibb.co/7Q89YnT/Honda-Wade.jpg',//Honda Wade - 20
+        sihinaGenenaKumariye: 'https://i.ibb.co/8j2c3DZ/Sihina-Genena-Kumariye.jpg',//Sihina Genena Kumariye - 21
+        sathya: 'https://i.ibb.co/m4nZ8F5/Sathya.jpg',//Sathya - 22
+        giriDevi: 'https://i.ibb.co/ccx4Rjh/Giridevi.jpg',//Giridevi - 23
+        garudaMuhurthaya: 'https://i.ibb.co/7YWtLpz/Garuda-Muhurthaya.jpg',//Garuda Muhurthaya - 24
+        igiBigi: 'https://i.ibb.co/TWyHH8c/IGI-BIGI.jpg',//IGI BIGI - 25
+        amaliya: 'https://i.ibb.co/QY7jdRh/AMALIYA.jpg',//AMALIYA - 26
+        ravanaSeason2: 'https://i.ibb.co/7by9XmM/ravana-season-2-1.jpg',//Ravana Season 02 - 27
+        apiApe: 'https://i.ibb.co/YhcGjsv/Api-Ape.jpg',//Api Ape - 28
+        naadi: 'https://i.ibb.co/s9hykM1/Naadi.jpg',//Naadi - 29
+        agniPiyapath: 'https://i.ibb.co/v4LdS2C/Agni-Piyapath.jpg',//Agni Piyapath - 30
+        sirasaTvKisa: 'https://i.ibb.co/gwFr4Xb/kisa.jpg',//Sirasa Tv Kisa - 31
+        sasaraKinnaravi: 'https://i.ibb.co/RTXMMjP/sasara-kinnaravi.jpg'//Sasara Kinnaravi - 32
+        };
+
+auk_dayly_tele_week = [
+  ['Season Ticket 301', '/2020/09/hiru-tv-season-ticket-ep301.html', 'seasonTicket'],
+  ['Aladdin 268', '/2020/09/swarnavahini-aladin-ep268.html', 'aladdin'],
+  ['Me Adarayai 1654', '/2020/09/sirasa-tv-me-adarayai-ep1654.html', 'meAdarayai'],
+  ['Sith Ahase Adaren 30', '/2020/09/hiru-tv-sith-ahase-adaren-ep30.html', 'sithAhaseAdaren'],
+  ['Adarei Man Adarei 1256', '/2020/09/hiru-tv-adarei-man-adarei-ep1256.html', 'adareiManAdarei'],
+  ['Sirasa Kisa 29', '/2020/09/sirasa-tv-kisa-ep29.html', 'sirasaTvKisa'],
+  ['Ahas Maliga 683', '/2020/09/hiru-tv-ahas-maliga-ep683.html', 'ahasMaliga'],
+  ['Deweni Inima 917', '/2020/09/tv-derana-deweni-inima-ep917.html', 'deweniInima'],
+  ['Sangeethe 378', '/2020/09/tv-derana-sangeethe-ep378.html', 'sangeethe'],
+  ['Dharani 14', '/2020/09/tv-derana-dharani-ep14.html', 'dharani'],
+  ['Neela Pabalu 587', '/2020/09/sirasa-tv-neela-pabalu-ep587.html', 'neelaPabalu'],
+  ['Sihina Samagama 88', '/2020/09/swarnavahini-sihina-samagama-ep88.html', 'sihinaSamagama'],
+  ['Hadawathe Kathawa 188', '/2020/09/swarnavahini-hadawathe-kathawa-ep188.html', 'hadawatheKathawa'],
+  ['Akuru Maki Na 64', '/2020/09/hiru-tv-akuru-maki-na-ep64.html', 'akuruMakiNa'],
+  ['Maha Viru Pandu 74', '/2020/09/hiru-tv-maha-viru-pandu-ep74.html', 'mahaViruPandu'],
+  ['Pulingu 41', '/2020/09/itn-pulingu-ep41.html', 'pulingu'],
+  ['Sihini 112', '/2020/09/itn-sihini-ep112.html', 'sihini'],
+  ['Sakala Guru 162', '/2020/09/rupavahini-sakala-guru-ep162.html', 'sakalaGuru'],
+  ['Honda Wade 29', '/2020/09/swarnavahini-honda-wade-ep29.html', 'hondaWade'],
+  ['Api Ape 39', '/2020/09/sirasa-api-ape-ep39.html', 'apiApe'],
+  ['ITN Naadi 31', '/2020/09/itn-naadi-ep31.html', 'naadi'],
+  ['Agni Piyapath 39', '/2020/09/swarnavahini-agini-piyapath-ep39.html', 'agniPiyapath']
+];
+
+auk_dayly_tele_week_end = [
+    ['Mini Pahana Obai 168', '/2020/09/tv-derana-mini-pahana-obai-ep168.html', 'miniPahanaObai'],
+  	['Sihina Genena Kumariye 72', '/2020/09/hiru-tv-sihina-genena-kumariye-ep72.html', 'sihinaGenenaKumariye'],
+    ['Agra 08', '/2020/09/sirasa-tv-agra-ep08.html', 'agra'],
+  	['Sathya 24', '/2020/09/itn-sathya-ep24.html', 'sathya'],
+  	['Giridevi 53', '/2020/09/itn-giridevi-ep53.html', 'giriDevi'],
+  	['Garuda Muhurthaya 11', '/2020/09/int-garuda-muhurthaya-ep11.html', 'garudaMuhurthaya'],
+  	['IGI BIGI 34', '/2020/09/swarnavahini-igi-bigi-ep34.html', 'igiBigi'],
+  	['AMALIYA 33', '/2020/09/swarnavahini-amaliya-ep33.html', 'amaliya'],
+  	['Ravana 45', '/2020/09/tv-derana-ravana-season-2-ep45.html', 'ravanaSeason2'],
+    ['Sasara Kinnaravi 14', '/2020/09/rupavahini-sasara-kinnaravi-ep14.html', 'sasaraKinnaravi']
+];
+
+auk_not_video = '<div style="background-image: linear-gradient(160deg,rgb(255, 21, 0), #920000);"><p style="text-align: center; font-size: 20px; font-weight: 900; color: #eee; padding: 20px 5px;">කතාව තාම දැම්මෙ නෑ. පසුව නැවත පැමිණෙන්න.</p></div>';
